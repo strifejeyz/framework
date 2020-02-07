48e94d1a90a99b2daa2b9a300ec9d51cd8c72934
Jesiener Canonigo <strifejeyz@gmail.com>
2020-02-07 21:12:28 +0800

    Moved to root directory from /public to fix hosting issues.

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Strife
+ * A Fast and Lightweight MVC Framework
+ *
+ * Author:  Jeyz Strife
+ * website: https://github.com/knyteblayde/strife
+ * Date:    11/10/2015
+ * Last Updated: 02/05/2020
+ */
+
+
+/**
+ * Require the composer autoloader
+ */
+require_once './vendor/autoload.php';
+
+
+/**
+ * Boot up the Strife Engine
+ */
+return new Kernel\Engine;
