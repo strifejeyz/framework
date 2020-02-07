83d5f0ca2c1ef5ea1895470821825847d916b6d0
Jesiener Canonigo <strifejeyz@gmail.com>
2020-02-07 20:46:47 +0800

    Added config for directories

---
+++ app/config/application.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * This is your project's main configuration file.
+ */
+
+
+/**
+ * Define the name for your app here, this will be used for
+ * template titles.
+ */
+define('APP_NAME', 'Strife App');
+
+
+/**
+ * Here you can define your default Controller
+ * class, when you visit '/' this will be called.
+ **/
+define('DEFAULT_CONTROLLER', 'HomeController');
+
+
+/**
+ * Here you can define your default method
+ **/
+define('DEFAULT_METHOD', 'index');
+
+
+/**
+ * You can use any kind of file type
+ * for template files so instead of
+ * render('/auth/login.php')
+ * you can render('/auth/login')
+ */
+define('TEMPLATE_TYPE', '.php');
+
+
+/**
+ * Application key will be used to further strengthen
+ * password stored on database.
+ * generate a new one at least before production
+ */
+define('APPLICATION_KEY', 'ttatjqoo1osroi5cc5s00xgypmurnvewcr381ypmqe7bi2kmkc');
+
+
+/**
+ * You can quickly switch to Maintenance mode.
+ */
+define('MAINTENANCE_MODE', FALSE);
+
+
+/**
+ * Directory where to save session files instead of server's
+ * default dir
+ * just omit if you want to use default dir.
+ */
+session_save_path('./storage/sessions/');
+
+
+/**
+ * Path to directories
+ * (Just in case you wanna move things around)
+ **/
+
+define('CONTROLLERS_PATH', '/app/controllers/');
+define('MIGRATIONS_PATH', '/app/migrations/');
+define('REQUESTS_PATH', '/app/requests/');
+define('SEEDERS_PATH', '/app/requests/');
+define('MODELS_PATH', '/app/models/');
+define('CONFIG_PATH', '/app/config/');
+define('VIEWS_PATH', '/app/views/');
+define('STORAGE_PATH', '/storage/');
+define('VENDOR_PATH', '/vendor/');
+
+
+
+
+
+
+
