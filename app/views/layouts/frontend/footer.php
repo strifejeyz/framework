f4d99b2c6c06076ce4f676e15e9aafdae3e98c2e
Jesiener Canonigo <strifejeyz@gmail.com>
2020-02-08 03:33:37 +0800

    downgraded from bootstrap 4.0 to 3.7

--- app/views/layouts/frontend/footer.php
+++ app/views/layouts/frontend/footer.php
@@ -1,3 +1,3 @@
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
