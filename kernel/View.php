f23e4b4e84f669bd5806ef6abdfa9d6fdcc31b49
Jesiener Canonigo <strifejeyz@gmail.com>
2020-02-08 18:57:27 +0800

    fixed caching pages issue when extract() is inaccessible to another method.

--- kernel/View.php
+++ kernel/View.php
@@ -45,10 +45,8 @@ abstract class View
         endif;
         $cachedFile = storage_path() . "/cache/" . md5($filename);
 
-        if (CACHED_VIEWS == true)
-        {
-            if (file_exists($cachedFile))
-            {
+        if (CACHED_VIEWS == true) {
+            if (file_exists($cachedFile)) {
                 return eval(' ?>' . self::evaluate($cachedFile) . '<?php ');
             } else {
                 $file = fopen($cachedFile, 'x');
@@ -61,111 +59,106 @@ abstract class View
     }
 
 
-/**
- * This is supposed to evaluate(locate and replace)
- * custom tags and generate cache for views.
- *
- * @param $filename
- * @return eval()
- **/
-private
-static function evaluate($filename)
-{
-    if (!file_exists($filename)) {
-        trigger_error("File does not exist '{$filename}'", E_USER_ERROR);
+    /**
+     * This is supposed to evaluate(locate and replace)
+     * custom tags and generate cache for views.
+     *
+     * @param $filename
+     * @return eval()
+     **/
+    private static function evaluate($filename)
+    {
+        if (!file_exists($filename)) {
+            trigger_error("File does not exist '{$filename}'", E_USER_ERROR);
+        }
+
+        $___ = file_get_contents($filename);
+        $___ = preg_replace('/\@extend\((.*)\)/', '<?php View::extend($1) ?>', $___);
+        $___ = preg_replace('/\@stop\((.*)\)/', '<?php View::stop($1) ?>', $___);
+        $___ = preg_replace('/\@parse\((.*)\)/', '<?php View::parse($1) ?>', $___);
+        $___ = preg_replace('/\@get\((.*)\)/', '<?php View::get($1) ?>', $___);
+        $___ = preg_replace('/\@import\((.*)\)/', '<?php use $1 ?>', $___);
+        $___ = preg_replace('/\{\{(.*)\}\}$/', '<?php echo htmlentities($1) ?>', $___);
+        $___ = preg_replace('/\{\{/', '<?php echo htmlentities($1', $___);
+        $___ = preg_replace('/\}\}/', ') ?>', $___);
+        $___ = preg_replace('/\{\!(.*)\!\}$/', '<?php echo $1 ?>', $___);
+        $___ = preg_replace('/\{\!/', '<?php echo $1', $___);
+        $___ = preg_replace('/\!\}/', ' ?>', $___);
+        $___ = preg_replace('/\\\{\\\{/', '{{', $___);
+        $___ = preg_replace('/\\\}\\\}/', '}}', $___);
+        $___ = preg_replace('/\{if (.*)\}/', '<?php if ($1) : ?>', $___);
+        $___ = preg_replace('/\{elseif (.*)\}/', '<?php elseif ($1) : ?>', $___);
+        $___ = preg_replace('/\{else\}/', '<?php else : ?>', $___);
+        $___ = preg_replace('/\{endif\}/', '<?php endif ?>', $___);
+        $___ = preg_replace('/\{for (.*)\}/', '<?php for ($1) : ?>', $___);
+        $___ = preg_replace('/\{endfor\}/', '<?php endfor ?>', $___);
+        $___ = preg_replace('/\{do\}/', '<?php do { ?>', $___);
+        $___ = preg_replace('/\{enddo (.*)\}/', '<?php } while($1) ?>', $___);
+        $___ = preg_replace('/\{while (.*)\}/', '<?php while ($1) : ?>', $___);
+        $___ = preg_replace('/\{endwhile\}/', '<?php endwhile ?>', $___);
+        $___ = preg_replace('/\{foreach (.*)\}/', '<?php foreach ($1) : ?>', $___);
+        $___ = preg_replace('/\{endforeach\}/', '<?php endforeach ?>', $___);
+
+        return ($___);
     }
 
-    $___ = file_get_contents($filename);
-    $___ = preg_replace('/\@extend\((.*)\)/', '<?php View::extend($1) ?>', $___);
-    $___ = preg_replace('/\@stop\((.*)\)/', '<?php View::stop($1) ?>', $___);
-    $___ = preg_replace('/\@parse\((.*)\)/', '<?php View::parse($1) ?>', $___);
-    $___ = preg_replace('/\@get\((.*)\)/', '<?php View::get($1) ?>', $___);
-    $___ = preg_replace('/\@import\((.*)\)/', '<?php use $1 ?>', $___);
-    $___ = preg_replace('/\{\{(.*)\}\}$/', '<?php echo htmlentities($1) ?>', $___);
-    $___ = preg_replace('/\{\{/', '<?php echo htmlentities($1', $___);
-    $___ = preg_replace('/\}\}/', ') ?>', $___);
-    $___ = preg_replace('/\{\!(.*)\!\}$/', '<?php echo $1 ?>', $___);
-    $___ = preg_replace('/\{\!/', '<?php echo $1', $___);
-    $___ = preg_replace('/\!\}/', ' ?>', $___);
-    $___ = preg_replace('/\\\{\\\{/', '{{', $___);
-    $___ = preg_replace('/\\\}\\\}/', '}}', $___);
-    $___ = preg_replace('/\{if (.*)\}/', '<?php if ($1) : ?>', $___);
-    $___ = preg_replace('/\{elseif (.*)\}/', '<?php elseif ($1) : ?>', $___);
-    $___ = preg_replace('/\{else\}/', '<?php else : ?>', $___);
-    $___ = preg_replace('/\{endif\}/', '<?php endif ?>', $___);
-    $___ = preg_replace('/\{for (.*)\}/', '<?php for ($1) : ?>', $___);
-    $___ = preg_replace('/\{endfor\}/', '<?php endfor ?>', $___);
-    $___ = preg_replace('/\{do\}/', '<?php do { ?>', $___);
-    $___ = preg_replace('/\{enddo (.*)\}/', '<?php } while($1) ?>', $___);
-    $___ = preg_replace('/\{while (.*)\}/', '<?php while ($1) : ?>', $___);
-    $___ = preg_replace('/\{endwhile\}/', '<?php endwhile ?>', $___);
-    $___ = preg_replace('/\{foreach (.*)\}/', '<?php foreach ($1) : ?>', $___);
-    $___ = preg_replace('/\{endforeach\}/', '<?php endforeach ?>', $___);
 
-    return ($___);
-}
+    /**
+     * Extends a view
+     *
+     * @param $layout
+     * @param $var
+     * @return self::render
+     */
+    public static function extend($layout, $var = [])
+    {
+        self::$layout = trim($layout, '/');
 
-
-/**
- * Extends a view
- *
- * @param $layout
- * @param $var
- * @return self::render
- */
-public
-static function extend($layout, $var = [])
-{
-    self::$layout = trim($layout, '/');
-
-    return self::render(self::$layout . '/header', $var);
-}
-
-
-/**
- * Require a footer file.
- *
- * @param $var
- * @return self::render
- */
-public
-static function stop($var = [])
-{
-    return self::render(self::$layout . '/footer', $var);
-}
-
-
-/**
- * Include a file and render custom tags.
- * similar to render
- *
- * @param $template
- * @param $var
- * @return self::render
- */
-public
-static function parse($template, $var = [])
-{
-    return self::render($template, $var);
-}
-
-
-/**
- * Include a file.
- * you cannot use custom tags with this.
- *
- * @param $template
- * @return self::render
- */
-public
-static function get($template)
-{
-    if (preg_match('/(.*)\.' . self::$postfix . '/i', $template)) {
-        $filename = '../views/' . $template;
-    } else {
-        $filename = '../views/' . $template . self::$postfix;
+        return self::render(self::$layout . '/header', $var);
     }
 
-    return include("$filename");
-}
+
+    /**
+     * Require a footer file.
+     *
+     * @param $var
+     * @return self::render
+     */
+    public static function stop($var = [])
+    {
+        return self::render(self::$layout . '/footer', $var);
+    }
+
+
+    /**
+     * Include a file and render custom tags.
+     * similar to render
+     *
+     * @param $template
+     * @param $var
+     * @return self::render
+     */
+    public static function parse($template, $var = [])
+    {
+        return self::render($template, $var);
+    }
+
+
+    /**
+     * Include a file.
+     * you cannot use custom tags with this.
+     *
+     * @param $template
+     * @return self::render
+     */
+    public static function get($template)
+    {
+        if (preg_match('/(.*)\.' . self::$postfix . '/i', $template)) {
+            $filename = '../views/' . $template;
+        } else {
+            $filename = '../views/' . $template . self::$postfix;
+        }
+
+        return include("$filename");
+    }
 }
