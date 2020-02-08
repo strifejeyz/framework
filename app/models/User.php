6611ff9892df13d329a81c3445b91087598c50bc
Jesiener Canonigo <strifejeyz@gmail.com>
2020-02-08 21:25:09 +0800

    Added documentation for possible properties.

--- app/models/User.php
+++ app/models/User.php
@@ -4,12 +4,20 @@ use Kernel\Database\QueryBuilder as Model;
 
 /**
  * Class User
+ *
  * @package App\Models
  */
 class User extends Model
 {
     /**
      * table name that will be used by this model
+     *
+     * you may also specify an Alias
+     * e.g. protected static $alias = "U";
+     *
+     * or specify a primary key like so
+     * protected static $primary_key = "U";
+     *
      * @var string
      */
     protected static $table = "users";
