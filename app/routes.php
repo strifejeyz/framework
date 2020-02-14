fca2776476c299b9fb8158f4f353e0a2f5705b9a
Jesiener Canonigo <strifejeyz@gmail.com>
2020-02-15 03:32:17 +0800

    Changed :: to @ for Class@method indicator

--- app/routes.php
+++ app/routes.php
@@ -4,31 +4,30 @@
  * List of route paths
  *
  * Here's an example of a simple route:
- * assign('MyTestRoute ->> /test', 'TestController::index()')
+ * assign('MyTestRoute ->> /test', 'TestController@index()')
  *
  * Explanation:
  * 'MyTestRoute' is a route name
  * '->>' is a pointer
  * '/test' is the URL
- * 'TestController::index()' is the Class and Method
+ * 'TestController@index()' is the Class and Method
  *
- * when Route::strict(true);
+ * when Route@strict(true);
  * e.g. /Foo is not equal to /foo
  */
 
-assign('welcome ->> /welcome', '/cms/WelcomeController::index');
-
+assign('welcome -> /welcome', '/cms/WelcomeController@index');
 
 
 /**
  * Authentication & Password
  * reset routes
  */
-assign('auth.login         ->> /login', 'AuthController::index');
-assign('auth.logout        ->> /logout', 'AuthController::logout');
-post('auth.attempt         ->> /attempt', 'AuthController::attempt');
-assign('auth.forgot.index  ->> /forgot-password', 'AuthController::forgotPassword');
-post('auth.forgot.email    ->> /forgot-password/send-email', 'AuthController::sendEmail');
-assign('auth.forgot.failed ->> /password-reset/failed', 'AuthController::passwordTokenFailed');
-post('auth.forgot.update   ->> /password-reset/update', 'AuthController::updatePassword');
-assign('auth.forgot.token  ->> /password-reset/:token', 'AuthController::verifyToken');
+assign('auth.login         -> /login', 'AuthController@index');
+assign('auth.logout        -> /logout', 'AuthController@logout');
+post('auth.attempt         -> /attempt', 'AuthController@attempt');
+assign('auth.forgot.index  -> /forgot-password', 'AuthController@forgotPassword');
+post('auth.forgot.email    -> /forgot-password/send-email', 'AuthController@sendEmail');
+assign('auth.forgot.failed -> /password-reset/failed', 'AuthController@passwordTokenFailed');
+post('auth.forgot.update   -> /password-reset/update', 'AuthController@updatePassword');
+assign('auth.forgot.token  -> /password-reset/:token', 'AuthController@verifyToken');
