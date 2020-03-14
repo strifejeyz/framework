c63cd6c8afb55c67840e4eeb79656b337ef0ce53
strifejeyz <strifejeyz@gmail.com>
2020-03-14 13:51:16 +0800

    renamed assign() to get()

--- app/routes.php
+++ app/routes.php
@@ -4,22 +4,21 @@
  * List of route paths
  *
  * Here's an example of a simple route:
- * assign('MyTestRoute ->> /test', 'TestController@index()')
+ * get('MyTestRoute -> /test', 'TestController@index()')
  */
 
-assign('welcome -> /welcome', '/cms/WelcomeController@index');
+get('welcome -> /welcome', '/cms/WelcomeController@index');
 
 
 
 /**
- * Authentication & Password
- * reset routes
+ * Authentication & Password reset
  */
-assign('auth.login         -> /login', 'AuthController@index');
-assign('auth.logout        -> /logout', 'AuthController@logout');
-post('auth.attempt         -> /attempt', 'AuthController@attempt');
-assign('auth.forgot.index  -> /forgot-password', 'AuthController@forgotPassword');
-post('auth.forgot.email    -> /forgot-password/send-email', 'AuthController@sendEmail');
-assign('auth.forgot.failed -> /password-reset/failed', 'AuthController@passwordTokenFailed');
-post('auth.forgot.update   -> /password-reset/update', 'AuthController@updatePassword');
-assign('auth.forgot.token  -> /password-reset/:token', 'AuthController@verifyToken');
+get('auth.login -> /login', 'AuthController@index');
+get('auth.logout -> /logout', 'AuthController@logout');
+post('auth.attempt -> /attempt', 'AuthController@attempt');
+get('auth.forgot.index -> /forgot-password', 'AuthController@forgotPassword');
+post('auth.forgot.email -> /forgot-password/send-email', 'AuthController@sendEmail');
+get('auth.forgot.failed -> /password-reset/failed', 'AuthController@passwordTokenFailed');
+post('auth.forgot.update -> /password-reset/update', 'AuthController@updatePassword');
+get('auth.forgot.token -> /password-reset/:token', 'AuthController@verifyToken');
