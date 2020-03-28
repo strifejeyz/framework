0e1db63be288a9705fbf12eb1b51ee69ad37eaca
strifejeyz <strifejeyz@gmail.com>
2020-03-28 20:57:03 +0800

    Added layout for backend

---
+++ app/views/layouts/backend/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+<head>
+    <title>{{$title}} &mdash; {{APP_NAME}}</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://bootswatch.com/3/slate/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Barlow+Condensed&display=swap">
+</head>
+<body>
