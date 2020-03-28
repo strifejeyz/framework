2098b58970315c2556613ebaef3c00ec9621d1e9
strifejeyz <strifejeyz@gmail.com>
2020-03-28 18:48:02 +0800

    updated landing page.

--- app/views/frontend/index.php
+++ app/views/frontend/index.php
@@ -1,14 +1,25 @@
 @extend('layouts/frontend')
 
-<img src="/assets/img/strife.png" width="130">
-<p class="title">Strife Framework</p>
-<p class="sub-title">A Fast and Lightweight PHP MVC Framework.</p>
+<div class="panel">
+    <div class="panel-header">
+        <ul>
+            <li class="red"></li>
+            <li class="yellow"></li>
+            <li class="green"></li>
+        </ul>
+    </div>
+    <div class="panel-content">
+        <img src="/assets/img/strife.png" width="130">
+        <p class="title">Strife Framework</p>
+        <p class="sub-title">A Fast and Lightweight PHP MVC Framework.</p>
 
-<ul class="list">
-    <li><a class="git"  href="https://github.com/strifejeyz/framework">Github</a></li>
-    <li><a class="site" href="http://strifeframework.ml">Official Site</a></li>
-    <li><a class="docs" href="http://strifeframework.ml/docs">Documentation</a></li>
-</ul>
+        <ul class="list">
+            <li><a class="git" href="https://github.com/strifejeyz/framework">Github</a></li>
+            <li><a class="site" href="http://strifeframework.ml">Official Site</a></li>
+            <li><a class="docs" href="http://strifeframework.ml/docs">Documentation</a></li>
+        </ul>
+    </div>
+</div>
 
 <div class="footer">&copy; 2020 Strife Framework. All rights reserved.</div>
 
