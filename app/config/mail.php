2416de63fde6fc91ff137c6199013c8e88ebebb5
strifejeyz <strifejeyz@gmail.com>
2021-03-16 10:50:26 +0800

    added config file for mailer

---
+++ app/config/mail.php
@@ -0,0 +1,35 @@
+<?php
+
+/**************************
+ * PHPMailer Config File  *
+ **************************/
+
+/**
+ * SMTP username
+ */
+define("SMTP_USERNAME", 'strifejeyz@gmail.com');
+
+
+/**
+ * SMTP password(usually, your email password)
+ */
+define("SMTP_PASSWORD", '010000040120');
+
+
+/**
+ * SMTP Host
+ * may be different per mail client
+ */
+define("SMTP_HOST", 'smtp.gmail.com');
+
+
+/**
+ * SMTP Port
+ */
+define("SMTP_PORT", 587);
+
+
+/**
+ * Use HTML email
+ */
+define("SMTP_USE_HTML", true);
