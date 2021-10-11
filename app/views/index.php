13fdf809f3ef78e12c79ed979864a63bee4069f0
Jesse <jesse@flatratecopiers.com>
2021-10-12 04:44:14 +0800

    updated to 8.0

--- app/views/index.php
+++ app/views/index.php
@@ -1,26 +1,9 @@
 @extend('layouts/frontend')
 
-<div class="panel">
-    <div class="panel-header">
-        <ul>
-            <li class="red"></li>
-            <li class="yellow"></li>
-            <li class="green"></li>
-        </ul>
-    </div>
-    <div class="panel-content">
-        <img src="assets/img/strife.png" width="130">
-        <p class="title">Strife Framework</p>
-        <p class="sub-title">A Fast and Lightweight PHP MVC Framework.</p>
-
-        <ul class="list">
-            <li><a class="git" href="https://github.com/strifejeyz/framework">Github</a></li>
-            <li><a class="site" href="http://strifeframework.cf">Official Site</a></li>
-            <li><a class="docs" href="http://docs.strifeframework.cf">Documentation</a></li>
-        </ul>
-    </div>
+<div class="strife">
+    <p class="logo">S</p>
+    <p class="title">Strife Framework <sup>PHP 8.0</sup></p>
+    <p class="sub-title">A Fast and Lightweight PHP MVC Framework.</p>
 </div>
-
-<div class="footer">&copy; 2020 Strife Framework. All rights reserved.</div>
 
 @stop()
