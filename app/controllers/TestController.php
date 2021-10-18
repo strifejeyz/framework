ed5dcc30e6e05ea29c14a71048eea521f7c69309
jesse-frc <jesse@flatratecopiers.com>
2021-10-18 12:08:08 +0800

    TestController.php for file-based routing

---
+++ app/controllers/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+class TestController
+{
+    /**
+     * Controller Index
+     * @return view
+     **/
+    public function index()
+    {
+        print "<h1>You have arrived!</h1>";
+    }
+}
